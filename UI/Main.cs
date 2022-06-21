818ce7cfb9b2dd16b83a1f02f12cae7f5ea11bac
Joaquín González <joaquin4525@hotmail.com>
2022-06-21 19:02:20 -0300

    Fix de traducir menú

--- UI/Main.cs
+++ UI/Main.cs
@@ -296,6 +296,7 @@ namespace UI
         {
             Traducir(idioma);
             TraducirMenu(idioma);
+            MostrarIdiomasDisponibles();
         }
 
         private void TraducirMenu(IIdioma idioma)
